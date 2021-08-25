57dda1ef3404f573a94b96afeb1ee99d4e21190c
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-25 12:46:16 -0500

    Te marca si es un número positivo, negativo o cero

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,6 +1,16 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    
+num=int(input("Dame un número "))
+if num==0:
+    print("Es cero")
+elif num > 0:
+    print("Es positivo")
+elif num<0:
+    print("Es negativo")
+            
+
     pass
     
 

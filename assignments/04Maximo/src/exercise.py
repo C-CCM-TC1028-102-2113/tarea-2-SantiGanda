ee924259c184be4434ef7ff4e64f7e378ac034d2
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-25 13:15:59 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    
+num1= int(input("Dame un número "))
+num2= int(input("Dame un número "))
+num3= int(input("Dame un número "))
+
+if num1>num2 and num1>num3:
+    print(str(num1))
+elif num2>num3 and num2>num1:
+    print(str(num2))
+else:
+    print(str(num3))
+
     pass
 
 

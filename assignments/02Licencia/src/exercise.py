ac1656c6a40c63fe89322b62ab1ba805f764befb
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-25 13:06:13 -0500

    Chequeo para revisar si el trámite de licencia se puede conceder

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,21 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    
+Edad=int(input("Dame tu edad "))
+Licencia=input("¿Tienes licencia? s o n ")
+
+if Edad < 18:
+    print("No cumples con los requisitos")
+
+elif Edad >= 18:
+        if Licencia == "s":
+            print("Trámite de la Licencia concedido")
+        elif Licencia == "n":
+            print("No cumples con los requisitos")
+        else:
+            print("Respuesta incorrecta")
+
     pass
 
 

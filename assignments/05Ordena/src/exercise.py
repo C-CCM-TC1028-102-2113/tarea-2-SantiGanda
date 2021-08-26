674c2c7b7d681dfbf0cce840e9ba67acc71a9032
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-25 22:43:07 -0500

    Ordena 3 números de mayor al menor

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,37 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    
+num1= int(input("Dame un número "))
+num2= int(input("Dame un número "))
+num3= int(input("Dame un número "))
+
+if num1>num2 and num1>num3:
+    print(str(num1))
+    if num2>num3:
+        print(str(num2))
+        print(str(num3))
+    else:
+        print(str(num3))
+        print(str(num2))
+
+elif num2>num1 and num2>num3:
+    print(str(num2))
+    if num3>num1:
+        print(str(num3))
+        print(str(num1))
+    else:
+        print(num1)
+        print(num3)
+
+elif num3>num2 and num3>num1:
+    print(num3)
+    if num2>num1:
+        print(num2)
+        print(num1)
+    else:
+        print(num1)
+        print(num2)
+
     pass
 
 
